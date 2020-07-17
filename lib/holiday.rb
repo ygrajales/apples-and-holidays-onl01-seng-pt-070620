523ca88335d797ca242ce763d71de950dab53959
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-17 03:45:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   
-  binding.pry
+  holiday_hash[:winter].values.flatten
 
 end
 
