5b048e57a484908d49da5dac08c4d17dc3384f3a
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-17 03:37:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
   
-  holiday_hash[:summer][:memorial_day] << "Grill"
+  holiday_hash[:summer][:memorial_day] << supply
   
   holiday_hash
 
