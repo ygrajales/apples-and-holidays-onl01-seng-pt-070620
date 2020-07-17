e9ad7d315612f873324dc01d1f42d92a77a851b3
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-17 03:32:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,7 +19,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # }
   # return the second element in the 4th of July array
   
-  holiday_hash[:summer][:fourth_of_july][2]
+  holiday_hash[:summer][:fourth_of_july][1]
   
 end
 
